b7a13038f0f7cd1e0f31aa0abbd926f08f7f16a5
Terje Sannum <terje.sannum@nav.no>
2017-05-14 19:30:45 +0200

    use gem extension

--- run-all.sh
+++ run-all.sh
@@ -8,7 +8,9 @@ docker run -d --rm --name=rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3.6.9-ma
 docker run -d --rm --name=sensu-api -p 4567:4567 --link redis --link rabbitmq -v $DIR/volumes/sensu/logs:/var/log/sensu terjesannum/sensu-api:2
 docker run -d --rm --name=uchiwa -p 3000:3000 --link sensu-api -v $DIR/volumes/uchiwa/config:/config uchiwa/uchiwa:0.24.0-1
 cat $DIR/metrics.influx | docker exec -i influxdb influx
-docker run -d --rm --name=sensu-server --link sensu-api --link redis --link rabbitmq --link influxdb -v $DIR/volumes/sensu/logs:/var/log/sensu terjesannum/sensu-server:4
+docker run -d --rm --name=sensu-server-metrics --link sensu-api --link redis --link rabbitmq --link influxdb -v $DIR/volumes/sensu/logs:/var/log/sensu terjesannum/sensu-server-metrics:1
+docker run -d --rm --name=sensu-server-events-s --link sensu-api --link redis --link rabbitmq --link influxdb -v $DIR/volumes/sensu/logs:/var/log/sensu terjesannum/sensu-server-events-s:1
+docker run -d --rm --name=sensu-server-events-n --link sensu-api --link redis --link rabbitmq --link influxdb -v $DIR/volumes/sensu/logs:/var/log/sensu terjesannum/sensu-server-events-n:1
 docker run -d --rm --name=sensu-client -p 3030:3030 --link rabbitmq -v $DIR/volumes/sensu/logs:/var/log/sensu terjesannum/sensu-client:2
 docker run -d --rm --name=grafana -p 3001:3000 --link influxdb -v $DIR/volumes/grafana/lib:/var/lib/grafana grafana/grafana:4.2.0
 $DIR/write-metrics.pl
---
+++ sensu-server-base/run.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+
+mkdir -p /etc/sensu/conf.d
+
+cat << EOF > /etc/sensu/conf.d/sensu.json
+{
+  "api": {
+    "host": "$SENSU_HOST",
+    "port": $SENSU_PORT
+  },
+  "handlers": {
+    "default": {
+      "type": "pipe",
+      "command": "cat"
+    }
+  },
+  "extensions": {
+    "influxdb": {
+      "gem": "sensu-extensions-influxdb",
+    }
+  }
+}
+EOF
+
+cat << EOF > /etc/sensu/conf.d/redis.json
+{
+  "redis": {
+    "host": "$REDIS_HOST",
+    "port": $REDIS_PORT
+  }
+}
+EOF
+
+cat << EOF > /etc/sensu/conf.d/rabbitmq.json
+{
+  "rabbitmq": {
+    "host": "$RABBITMQ_HOST",
+    "port": $RABBITMQ_PORT,
+    "vhost": "$RABBITMQ_VHOST",
+    "user": "$RABBITMQ_USER",
+    "password": "$RABBITMQ_PASSWORD"
+  }
+}
+EOF
+
+cat << EOF > /etc/sensu/conf.d/influxdb-extension.json
+{
+  "influxdb-extension": {
+    "hostname": "$INFLUXDB_HOST",
+    "port": $INFLUXDB_PORT,
+    "database": "$INFLUXDB_DATABASE",
+    "username": "$INFLUXDB_USER",
+    "password": "$INFLUXDB_PASSWORD",
+    "buffer_size": $BUFFER_SIZE
+  }
+}
+EOF
+
+exec /opt/sensu/bin/sensu-server -d /etc/sensu/conf.d -l /var/log/sensu/server-$HOSTNAME.log -L $LOG_LEVEL
