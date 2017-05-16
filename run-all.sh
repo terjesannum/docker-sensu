5886b6ddd05bcbd24cd8a44a8b9acf8ae6d979ab
Terje Sannum <terje.sannum@nav.no>
2017-05-16 19:34:18 +0200

    fill measurements with some initial data

--- run-all.sh
+++ run-all.sh
@@ -11,4 +11,6 @@ cat $DIR/metrics.influx | docker exec -i influxdb influx
 docker run -d --rm --name=sensu-server --link sensu-api --link redis --link rabbitmq --link influxdb -v $DIR/volumes/sensu/logs:/var/log/sensu terjesannum/sensu-server:5
 docker run -d --rm --name=sensu-client -p 3030:3030 --link rabbitmq -v $DIR/volumes/sensu/logs:/var/log/sensu terjesannum/sensu-client:2
 docker run -d --rm --name=grafana -p 3001:3000 --link influxdb -v $DIR/volumes/grafana/lib:/var/lib/grafana grafana/grafana:4.2.0
+# write some initial data to influxdb
 $DIR/write-metrics.pl
+$DIR/write-metrics.pl --measurement random --tag hostname --series 1 --total 1000 --base 1 --scale 50
